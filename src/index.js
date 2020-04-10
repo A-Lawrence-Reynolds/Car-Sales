aa8cd054b14b1ad8d4a5650b9b7c4df44386fa6e
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-04-10 09:42:49 -0700

    made the redux store and provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import {provider} from 'react-redux';
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { createStore } from "redux";
+
+const store = createStore(reducer)
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<provider store={store}>
+  <App />  
+</provider>
+, rootElement);
