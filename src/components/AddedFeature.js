159d0b7aeda295419f9dec9ecace8c135eba098b
Adam-Reynolds <a-lawrence-reynolds@protonmail.com>
2020-04-10 11:15:44 -0700

    making head way on the reducer

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,19 @@
 import React from 'react';
-
+import {connect} from "react-redux"
+import {REMOVE} from '../actions/index-actions '
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button className="button" onClick={() => props.remove(prop.feature)}>X</button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+export default connect(
+  state = ({}),
+  dispatch => ({
+    remove: feature => dispatch({type:REMOVE, payload: feature})
+  })
+)(AddedFeature);
